4c28e32f69a43817d67dc7df727dcb832768ebab
Anthony <anthonymellon@yahoo.com>
2024-06-15 02:20:46 +1200

    Rewrite Board to use two loops to make the squares instead of hardcoding them

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ function Square({value, onSquareClick}) {
 
 function Board({xIsNext, squares, onPlay}) {
 
+  const numRows = Math.sqrt(squares.length);
+  const numCols = numRows; //just for readability, I think it's nice :)
+
   function handleClick(i) {
     if(calculateWinner(squares) || squares[i])
     {
@@ -37,26 +40,23 @@ function Board({xIsNext, squares, onPlay}) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  const boardReturn = [];
+  //let count = 0; //Why doesn't count work? this gave me so much grief >:(
+  for(let x = 0; x < numRows; x++) {
+    const row = [];
+    for(let y = 0; y < numCols; y++) {        
+        row.push(<Square value={squares[3*y + x /*count*/]} onSquareClick={() => handleClick(3*y + x /*count*/)} key={y}/>)
+        //count++;
+      }
+      boardReturn.push(<div className="board-row" key={x}>{row}</div>);
+  }
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-      </div>
+      {boardReturn}
     </>
-  )
+  );
 }
 
 export default function Game() {
